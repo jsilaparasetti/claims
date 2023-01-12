b6e3c913cbca92013cdf411398fdeb350852914a
Apurva Bansode <45214076+apurva199@users.noreply.github.com>
2023-01-12 12:35:53 -0500

    Update ClaimsApplication.java

--- src/main/java/com/miracle/claims/ClaimsApplication.java
+++ src/main/java/com/miracle/claims/ClaimsApplication.java
@@ -2,8 +2,8 @@ package com.miracle.claims;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
